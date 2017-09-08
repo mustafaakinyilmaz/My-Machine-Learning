0e8ea7e965977af4a117a796bddd86e8f9a626ca
Akın Yılmaz <mustafaakinyilmaz@gmail.com>
2017-09-08 18:06:29 +0300

    Update forecast.py

--- btc_dollar_forecast/forecast.py
+++ btc_dollar_forecast/forecast.py
@@ -1,6 +1,9 @@
 
-from classes import NARNET
+from classes import *
+import time
 
+print("\nThis Neural Network has only 1 hidden layer!\n")
+time.sleep(1)
 net = NARNET(filename='btc_usd', numofNeurons=4, numofDelayedInputs=3, hidden_activation='lin', out_activation='lin', numofEpochs=50000)
 net.train()
 net.predict()
