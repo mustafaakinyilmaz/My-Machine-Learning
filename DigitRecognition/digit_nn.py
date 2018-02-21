a156a88852afa8e7f05012651c42dbb83208e728
Akın Yılmaz <mustafaakinyilmaz@gmail.com>
2018-02-21 16:50:04 +0300

    Update digit_nn.py
    Two hidden layer NN.
    Altered ReLu activation.

--- DigitRecognition/digit_nn.py
+++ DigitRecognition/digit_nn.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan 15 12:45:49 2018
+Created on Wed Feb 21 12:00:12 2018
 
 @author: akinyilmaz
 """
-"""log = np.log(l_out)
-mul = -(log*Y_train)
-cross_ent = mul.sum(axis=1,keepdims=True)"""
+
 
 import pandas as pd
 import numpy as np
@@ -20,10 +18,24 @@ def sigmoid(inp,deriv=False):
     else:
         return fx
 
-def relu(inp,deriv=False):
-    fx = inp * (inp > 0)
+def tanh(inp,deriv=False):
+    fx = (np.exp(inp)-np.exp(-inp)) / (np.exp(inp)+np.exp(-inp))
     if deriv == True:
-        return (inp>0)*1
+        return 1-fx*fx
+    else:
+        return fx
+    
+
+def relu(inp,deriv=False):
+    k1 = 0.01
+    k2 = 0.5
+    up = 20
+    fxplus = k2*inp*(inp>0)*(inp<up)
+    fxplus = fxplus + np.max(fxplus)*(inp>=up)
+    fxminus = k1*inp*(inp<0)
+    fx = fxplus + fxminus
+    if deriv == True:
+        return (inp<up)*(inp>0)*k2 + (inp<=0)*k1
     else:
         return fx
 
@@ -38,7 +50,6 @@ def softmax(inp,deriv=False):
         return soft
 
 
-
 dataset = pd.read_csv('train.csv',sep=',').values
 from sklearn.model_selection import train_test_split
 train_set,test_set = train_test_split(dataset,test_size=0.2,random_state=0)
@@ -51,10 +62,12 @@ train_results = train_set[:,0].reshape(-1,1)
 test_features = test_set[:,1:]
 test_results = test_set[:,0].reshape(-1,1)
 
-from sklearn.preprocessing import MinMaxScaler
-scaler = MinMaxScaler(feature_range=(0,1))
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
 train_features = scaler.fit_transform(train_features)
 test_features = scaler.transform(test_features)
+"""train_features = train_features/255
+test_features = test_features/255"""
 
 
 from sklearn.preprocessing import OneHotEncoder
@@ -74,24 +87,31 @@ Y_test = test_results
 
 
 neuron_inp_size = np.shape(X_train)[1]
-neuron_hidden_size = 200
+neuron_hidden1_size = 500
+neuron_hidden2_size = 400
 neuron_out_size = 10
 
 
 
-W1 = np.random.randn(neuron_inp_size,neuron_hidden_size)
-W2 = np.random.randn(neuron_hidden_size,neuron_out_size)
-b1 = np.random.randn(neuron_hidden_size,)
-b2 = np.random.randn(neuron_out_size,)
+W1 = np.random.randn(neuron_inp_size,neuron_hidden1_size)*0.01
+W2 = np.random.randn(neuron_hidden1_size,neuron_hidden2_size)*0.01
+W3 = np.random.randn(neuron_hidden2_size,neuron_out_size)
+b1 = np.random.randn(1,neuron_hidden1_size)
+b2 = np.random.randn(1,neuron_hidden2_size)
+b3 = np.random.randn(1,neuron_out_size)
 
+nb_epoch = 250
+batch_size = 128
+learning_rate0 = 1.e-2
 
-batch_size = 50
-nb_epoch = 50
-learning_rate = 7.e-4
+epsilon = 1.e-7
 
-cost_list = np.array([],dtype=float)
+train_accur_list = []
+test_accur_list = []
+epoch_list = []
 
 for epoch in range(1,nb_epoch+1): 
+    learning_rate = learning_rate0 - (((epoch-1)**2)/(nb_epoch**2))*learning_rate0
     for batch in range(0,np.shape(X_train)[0],batch_size):
         if np.shape(X_train)[0]-batch < batch_size:
             l_inp = X_train[batch:]
@@ -101,48 +121,59 @@ for epoch in range(1,nb_epoch+1):
             Y_real = Y_train[batch:batch+batch_size]
 
 
-        s_hidden = np.dot(l_inp,W1) + b1
-        l_hidden = sigmoid(s_hidden)
-        s_out = np.dot(l_hidden,W2) + b2
+        s_hidden1 = np.dot(l_inp,W1) + b1
+        l_hidden1 = relu(s_hidden1)
+        
+        s_hidden2 = np.dot(l_hidden1,W2) + b2
+        l_hidden2 = relu(s_hidden2)
+        
+        s_out = np.dot(l_hidden2,W3) + b3
         l_out = softmax(s_out)
 
-
-        l_out_error = (l_out - Y_real)*softmax(s_out,deriv=True)
-        W2_delta = learning_rate*np.dot(l_hidden.T,l_out_error)
-        b2_delta = learning_rate*np.sum(l_out_error,axis=0)
         
-        l_hidden_error = np.dot(l_out_error,W2.T)*sigmoid(s_hidden,deriv=True)
-        W1_delta = learning_rate*np.dot(l_inp.T,l_hidden_error)
-        b1_delta = learning_rate*np.sum(l_hidden_error,axis=0)
+        dloss = 2*(l_out-Y_real)
+        #dloss = -(Y_real/(l_out+epsilon))
+        
+        l_out_error = (dloss*softmax(s_out,deriv=True))/(l_inp.shape[0])
+        W3_delta = learning_rate*np.dot(l_hidden2.T,l_out_error)
+        b3_delta = learning_rate*np.sum(l_out_error,axis=0).reshape(1,-1)
+        
+        l_hidden2_error = np.dot(l_out_error,W3.T)*relu(s_hidden2,deriv=True)
+        W2_delta = learning_rate*np.dot(l_hidden1.T,l_hidden2_error)
+        b2_delta = learning_rate*np.sum(l_hidden2_error,axis=0).reshape(1,-1)
+        
+        l_hidden1_error = np.dot(l_hidden2_error,W2.T)*relu(s_hidden1,deriv=True)
+        W1_delta = learning_rate*np.dot(l_inp.T,l_hidden1_error)
+        b1_delta = learning_rate*np.sum(l_hidden1_error,axis=0).reshape(1,-1)
+        
+        
+        W3 -= W3_delta
+        b3 -= b3_delta
         W2 -= W2_delta
         b2 -= b2_delta
         W1 -= W1_delta
         b1 -= b1_delta
         
     
-    if epoch > 2:
-        momentum = cost_list[0]/cost_list[1]
-        learning_rate *= momentum
-        cost_list = cost_list[1]
     
-    s_whole_hidden = np.dot(X_train,W1) + b1
-    l_whole_hidden = sigmoid(s_whole_hidden)
-    s_whole_out = np.dot(l_whole_hidden,W2) + b2
-    l_whole_out = softmax(s_whole_out)
-    train_error = np.sum(np.square(l_whole_out-Y_train))/np.shape(Y_train)[0]
-    cost_list = np.append(cost_list,train_error)
+
+    Y_hat_train = softmax(np.dot(relu(np.dot(relu(np.dot(X_train,W1)+b1),W2)+b2),W3)+b3)
+    train_error = np.sum(np.square(Y_hat_train-Y_train))/(Y_train.shape[0]*2)
+    #train_error = -np.sum(Y_train*np.log(Y_hat_train+epsilon))/(Y_train.shape[0])
+    
     
     real = np.argmax(Y_train,axis=1)
-    predicted = np.argmax(l_whole_out,axis=1)
+    predicted = np.argmax(Y_hat_train,axis=1)
     
     
     
-    test_whole_hidden = sigmoid(np.dot(X_test,W1) + b1)
-    test_whole_out = softmax(np.dot(test_whole_hidden,W2) + b2)
+    Y_hat_test = softmax(np.dot(relu(np.dot(relu(np.dot(X_test,W1)+b1),W2)+b2),W3)+b3)
     
-    test_error = np.sum(np.square(test_whole_out-Y_test))/np.shape(Y_test)[0]
+    test_error = np.sum(np.square(Y_hat_test-Y_test))/(np.shape(Y_test)[0]*2)
+    #test_error = -np.sum(Y_test*np.log(Y_hat_test+epsilon))/(Y_test.shape[0])
+    
     test_real = np.argmax(Y_test,axis=1)
-    test_predicted = np.argmax(test_whole_out,axis=1)
+    test_predicted = np.argmax(Y_hat_test,axis=1)
     
 
     
@@ -160,34 +191,36 @@ for epoch in range(1,nb_epoch+1):
     train_accurracy = correct_train /  len(real)
     test_accurracy = correct_test / len(test_real)
     
+    epoch_list.append(epoch)
+    train_accur_list.append(100*train_accurracy)
+    test_accur_list.append(100*test_accurracy)
+    
     print("epoch: "+ str(epoch))
     print("learning rate: "+ str(learning_rate))
     print("train error: "+ str(train_error))
-    print("train accurracy: "+ str(train_accurracy))
+    print("train accurracy: "+ str(train_accurracy)+" ("+str(correct_train)+"/"+str(len(real))+")")
     print("test error: " + str(test_error))
-    print("test accurracy: "+ str(test_accurracy))
+    print("test accurracy: "+ str(test_accurracy)+" ("+str(correct_test)+"/"+str(len(test_real))+")")
     print("-------------------------------------")
 
+  
     
-
-"""test_dataset = pd.read_csv('test.csv',sep=',').values
-test_dataset = scaler.transform(test_dataset)
-c = input('Want to test? (y/n)')
-
-while c.lower() == 'y':
-    indice = np.random.randint(0,np.shape(test_dataset)[0],1)
-    print(indice)
-    sample = test_dataset[indice]
-    image_array = np.reshape(sample,newshape=(28,28))
-    
-    test_hidden = sigmoid(np.dot(sample,W1) + b1)
-    test_output = softmax(np.dot(test_hidden,W2) + b2)
-    predicted_test = np.argmax(test_output,axis=1)
-    #print("prediction: " + str(predicted_test))
-    plt.title('prediction: ' + str(predicted_test))
-    plt.imshow(image_array,cmap='Greys')
-    time.sleep(2)
-    plt.close()"""
+plt.plot(epoch_list,train_accur_list,'-b',label="training accurracy")
+plt.plot(epoch_list,test_accur_list,'-r',label="test accurracy")
+plt.plot(epoch_list,list(np.ones((len(epoch_list)))*99.5),'-g',label="human level accurracy")
+plt.xlabel("# of iterations")
+plt.ylabel("% accurracy")
+plt.legend()
+plt.grid()
+plt.show()
 
 
-    
+indice = 6
+image_array = scaler.inverse_transform(train_features[indice,:]).reshape(28,28)
+plt.imshow(image_array,cmap='Greys')
+
+test_output = softmax(np.dot(relu(np.dot(relu(np.dot(train_features[indice,:],W1)+b1),W2)+b2),W3)+b3)
+predicted_result = np.argmax(test_output,axis=1) 
+print(predicted_result)
+
+
